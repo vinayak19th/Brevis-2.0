794f244862952e5eaa9a57dc3beca43544b8fe8c
vinayak19th <vinayak1998th@gmail.com>
2021-04-14 02:33:51 +0530

    NLP fix

--- brevis/predict/bart.py
+++ brevis/predict/bart.py
@@ -1,75 +1,110 @@
+import random
+import os
+import sys
+
+import newspaper
+
+from tools.trie import *
+
 import requests
-import json
-from transformers import BartTokenizer
+from bs4 import BeautifulSoup
+from newsapi import NewsApiClient
 
-class SummaryModel:
-    model = "facebook/bart-large-xsum"
-    tokenizer = BartTokenizer.from_pretrained(model)
-    print("Tokenizer loaded")
+# NUM_POPULAR_URLS = 3
+# NUM_EACH_POPULAR = 2
+# FORGET_ARTICLE = False
 
-    def __init__(self):
-        print("model loaded")
+NUM_ARTICLES = 20
 
-    def preprocess(self,text,maxlen):
-            """Function to pre-process text inputs
+BASE_DIR = Path(__file__).resolve().parent.parent
+SAVED_TRIE_DIR = BASE_DIR / 'tools' / 'savedtries'
 
-            Args:
-                text [str]: Text to be summarized
+def get_articles():
+    newsapi = NewsApiClient(api_key='d84cf1257d084ed3b9eec34250c389ca')
 
-            Returns:
-                [list]: tokens to pass to the predictor
-            """
-            #Pre-Process Data
-            assert(type(text) == str),"Text being passed is not a string"
-            assert(type(maxlen) == int),"Maxlen must be an integer"
-            print("datatype Text confirmed")
-            tokens = SummaryModel.tokenizer(text) 
-            print("Tokens generated")
+    all_articles_response = newsapi.get_everything(sources='bbc-news,the-verge',
+                                        domains='bbc.co.uk,techcrunch.com',
+                                        language='en',
+                                        sort_by='relevancy')
 
-            batch = dict(tokens)
-            batch["maxlen"] = maxlen
-            batch = [batch]
+    title_content_dict = {}
+    articles = all_articles_response['articles']
+
+    for i in range(NUM_ARTICLES):
+        article = articles[i]
+        title = article['title']
+        if title in title_content_dict:
+            continue
+
+        html = requests.get(article['url'])
+        soup = BeautifulSoup(html.text, 'html.parser')
+
+        content = [p_tag.get_text() for p_tag in soup.find_all('p')]
+        content = '\n'.join(content)
+
+        title_content_dict[title] = {'content':content,'url':article['url']}
+
+    return title_content_dict
+
+
+
+# def forget_articles(url):
+#     print(f"Forgettig {url} articles")
+#     domain = \
+#         url.replace("https://", "http://").replace("http://", "").split("/")[0]
+#     d_pth = os.path.join(newspaper.settings.MEMO_DIR, domain + ".txt")
+#     if os.path.exists(d_pth):
+#         os.remove(d_pth)
+
+# def get_articles():
+
+#     word_checker = Trie()
+#     print(SAVED_TRIE_DIR / 'trie.pkl')
+#     print("Loading word checker...")
+#     word_checker.load(SAVED_TRIE_DIR / 'trie.pkl')
+
+#     print(f"Obtaining {NUM_POPULAR_URLS} popular URLs")
+#     populars = newspaper.popular_urls()[:NUM_POPULAR_URLS]
+
+#     for p in populars:
+#         if FORGET_ARTICLE:
+#             forget_articles(p)
+#         print(p)
+
+#     print("Building popular newspapers...")
+#     popular_newspaper_build = []
+#     for idx, p in enumerate(populars):
+#         print(f"Building {idx + 1} \t {p}")
+#         popular_newspaper_build.append(newspaper.build(p, memoize_articles = False, language='en'))
+
+#     print("Getting articles text list...")
+#     articles_text_list = []
+#     title_article_dict = {}
+
+#     for pb in popular_newspaper_build:
+#         size = len(pb.articles)
+#         print(f"{pb.brand} has {size} articles.")
+#         for _ in range(NUM_EACH_POPULAR):
             
-            #Send Server Request
-            input_data = {"signature_name": "serving_default","instances": batch}
-            input_data = json.dumps(input_data)
-            return input_data
-        
-    def pred(self,text,maxlen=300,tokens_only=False):
-        """Returns summaries from model
+#             while True:
+#                 index = random.randint(0, size-1)
+#                 print(index, end = ' ')
+#                 article = pb.articles[index]
+#                 try:
+#                     article.download()
+#                     article.parse()
+#                     title = article.title
+#                     text = article.text
 
-        Args:
-            text ([str]): text to be summarized
-            tokens_only (bool, optional): Flag to set decoding returns. Defaults to False.
+#                     if not text:
+#                         raise Exception('')
+#                 except:
+#                     continue
+                
+#                 print(text, word_checker.test_corpus__(text))
+#                 title_article_dict[title] = text
+#                 # articles_text_list.append(text)
+#                 break
+#         print()
 
-        Returns:
-            [list]: returns summary text if 'tokens_only' set to false, 
-                    else returns summary tokens
-        """
-        input_data = self.preprocess(text,maxlen)
-        print("Data pre-processed")
-        r = requests.post("http://localhost:8501/v1/models/bart:predict", data=input_data)
-        print("Model inference complete")
-        flag = False
-        try:
-            output_tokens = json.loads(r.text)['predictions']
-        except KeyError:
-            output_tokens = "ERROR:" + json.loads(r.text)['error']
-            flag = True
-
-        if(tokens_only or flag):
-            return output_tokens
-        else:
-            return self.decoder(output_tokens)
-
-    def decoder(self,tokens):
-            """Returns summary text from summary
-
-            Args:
-                tokens ([list]): summary tokens
-
-            Returns:
-                [list]: list containing the string summary
-            """
-            result = [SummaryModel.tokenizer.decode(g, skip_special_tokens=False, clean_up_tokenization_spaces=False) for g in tokens]
-            return result
+#     return title_article_dict
