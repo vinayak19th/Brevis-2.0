4220f54bb3e2fe76b2bed3f0c56ea345aa33b062
vinayak19th <vinayak1998th@gmail.com>
2021-04-14 02:39:40 +0530

    Updated urls

--- brevis/predict/urls.py
+++ brevis/predict/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-
 from . import views
 
+
 urlpatterns = [
-    path('output/', views.summariser, name='summariser'),
-    path('', views.index, name='index')
+    path('', views.ArticleListView.as_view(), name='article_list'),
+    path('<slug:slug>', views.ArticleDetailView.as_view(), name='article_detail')
+    #path('', views.index, name='index')
 ]
+
+from .startup import run_startup
+run_startup()     
